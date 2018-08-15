192da20e33b0d6ed1d2e6213bfee50223fc7bff1
Darrell Pawson <dpawson905@gmail.com>
2018-08-15 18:13:58 -0400

    add vuex and work on login and header

--- client/src/main.js
+++ client/src/main.js
@@ -4,17 +4,23 @@ import Vue from "vue";
 import App from "./App";
 import router from "./router";
 import Vuetify from "vuetify";
+import { sync } from "vuex-router-sync";
 import "vuetify/dist/vuetify.min.css";
 import "vuetify/dist/vuetify.min.js";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
+import store from "./store/store";
+
 Vue.config.productionTip = false;
 
 Vue.use(Vuetify);
+
+sync(store, router);
 
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
   router,
+  store,
   components: { App },
   template: "<App/>"
 });
